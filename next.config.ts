112af75b8bdcd363f3f39d07469fd02b671a7631
Minh Nhi <minhnhi15102003@gmail.com>
2025-12-02 10:34:23 +0700

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/webpublic-fashion-shop",
+  assetPrefix: "/webpublic-fashion-shop/",
 };
 
 export default nextConfig;
